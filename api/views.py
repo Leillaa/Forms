3b3c3b4423de5e1c56d4d8784739a7640ca23edd
SergSukh <s9626265386@gmail.com>
2023-12-19 13:14:51 +0300

    updated api

--- api/views.py
+++ api/views.py
@@ -1,11 +1,5 @@
 from django.contrib.auth import get_user_model
-from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import status, viewsets
-from rest_framework.decorators import action
-from rest_framework.generics import get_object_or_404
-from rest_framework.permissions import AllowAny, IsAuthenticated
-from rest_framework.response import Response
-from rest_framework.views import APIView
+from rest_framework import viewsets
 
 from survey.models import Survey, Answer
 from questionnaire.models import Question, Questionnaire, Point
